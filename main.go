bc47d3984a96c1431b42b4d5afeff60a21791fc7
xavier-rdo <xavier.roldo@elao.com>
2015-05-02 17:27:09 +0200

    Scaffold config & converters in main

--- main.go
+++ main.go
@@ -3,24 +3,76 @@ package main
 import (
     "flag"
     "fmt"
+    "regexp"
 )
 
 var url string
 
 type UrlConverter struct {
-    origin string
-    destination string
+	origin      string
+	destination string
 }
 
-type UrlConfig struct {
-    urlConverter UrlConverter
+type ParsingRule struct {
+	urlPattern   string
+	ruleType     string
+	contentTypes []string
+	path         string
+	field        string
+}
+
+type DomainConfig struct {
+	converters   []UrlConverter
+    parsingRules []ParsingRule
 }
 
 func init() {
-    flag.StringVar(&url, "url", "", "Requested URL")
-    flag.Parse()
+	flag.StringVar(&url, "url", "", "Requested URL")
+	flag.Parse()
 }
 
 func main() {
-    fmt.Println("URL is ", url)
+	fmt.Println("URL is ", url)
+
+	urlConverter := UrlConverter{
+		"^http(?:s)?:\\/\\/github\\.com\\/([a-zA-Z0-9\\-_]+)$",
+		"https://api.github.com/users/$1",
+	}
+	fmt.Printf("my urlConverter: %v\n", urlConverter)
+	converters := make([]UrlConverter, 0, 2)
+	converters = append(converters, urlConverter)
+	fmt.Printf("my urlConverters: %v\n", converters)
+
+    rules := make([]ParsingRule, 0)
+    rule := ParsingRule{
+        "^https:\\/\\/api\\.github\\.com\\/users\\/",
+        "json-pointer",
+        []string{"application/json"},
+        "/name",
+        "title",
+    }
+    rules = append(rules, rule);
+
+	domainConfig := DomainConfig{
+		converters,
+        rules,
+	}
+	fmt.Printf("domainConfig: %v\n", domainConfig)
+
+    fmt.Printf("Converter for URL: %v", findConverter(url, domainConfig))
+}
+
+func findConverter(url string, config DomainConfig) *UrlConverter {
+   for _, converter := range config.converters {
+        matched, err := regexp.MatchString(converter.origin, url)
+        if matched {
+            return &converter
+        }
+        if err != nil {
+            fmt.Printf("err %v", err)
+            return nil
+        }
+   }
+
+   return nil
 }
