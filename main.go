31e892be13b6488b976f3f558f51286d8e3f127a
XRoldo <xavier.roldo@elao.com>
2015-05-08 16:00:09 +0200

    main func refactoring

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "regexp"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
 )
 
 var url string
@@ -21,13 +23,15 @@ type ParsingRule struct {
 	field        string
 }
 
+// config for domain (converters, parsing rules, credentials, etc.)
 type DomainConfig struct {
 	converters   []UrlConverter
-    parsingRules []ParsingRule
+	parsingRules []ParsingRule
 }
 
+// config for input URL ; parsing rules here are a filtered subset of domain's parsing rules
 type UrlConfig struct {
-    parsingRules []ParsingRule
+	parsingRules []ParsingRule
 }
 
 func init() {
@@ -38,6 +42,78 @@ func init() {
 func main() {
 	fmt.Println("URL is ", url)
 
+	domainConfig := mockGithubConfig()
+
+	if converter := findConverter(url, domainConfig); converter != nil {
+		url = convertUrl(url, converter)
+		fmt.Println(url)
+		domainConfig = mockGithubConfig()
+	}
+
+	urlConfig := buildUrlConfig(url, &domainConfig)
+	fmt.Printf("%v\n", urlConfig.parsingRules)
+
+	//@TODO: http call to URL
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+	//@TODO: apply parsing rules
+	// defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+	fmt.Print(body)
+	//@TODO: return OG-like structure
+
+}
+
+// @TODO: a method of the UrlConverter struct ?
+func findConverter(url string, config DomainConfig) *UrlConverter {
+	for _, converter := range config.converters {
+		matched, err := regexp.MatchString(converter.origin, url)
+		if matched {
+			return &converter
+		}
+		if err != nil {
+			//@TODO: do something with err (log.error)
+			fmt.Printf("err %v", err)
+			return nil
+		}
+	}
+
+	return nil
+}
+
+// @TODO: a method of the UrlConverter struct ?
+func convertUrl(url string, converter *UrlConverter) string {
+	re := regexp.MustCompile(converter.origin)
+	return re.ReplaceAllString(url, converter.destination)
+}
+
+func buildUrlConfig(url string, domainConfig *DomainConfig) *UrlConfig {
+	rules := make([]ParsingRule, 0)
+	for _, rule := range domainConfig.parsingRules {
+		fmt.Printf("%s\n", rule.urlPattern)
+		matched, err := regexp.MatchString(rule.urlPattern, url)
+		if matched {
+			rules = append(rules, rule)
+		}
+		if err != nil {
+			//@TODO: do something with err (log.error)
+			fmt.Printf("err %v", err)
+			return nil
+		}
+	}
+
+	urlConfig := &UrlConfig{rules}
+	return urlConfig
+}
+
+func mockGithubConfig() DomainConfig {
 	urlConverter := UrlConverter{
 		"^http(?:s)?:\\/\\/github\\.com\\/([a-zA-Z0-9\\-_]+)$",
 		"https://api.github.com/users/$1",
@@ -47,71 +123,21 @@ func main() {
 	converters = append(converters, urlConverter)
 	fmt.Printf("my urlConverters: %v\n", converters)
 
-    rules := make([]ParsingRule, 0)
-    rule := ParsingRule{
-        "^https:\\/\\/api\\.github\\.com\\/users\\/",
-        "json-pointer",
-        []string{"application/json"},
-        "/name",
-        "title",
-    }
-    rules = append(rules, rule);
+	rules := make([]ParsingRule, 0)
+	rule := ParsingRule{
+		"^https:\\/\\/api\\.github\\.com\\/users\\/",
+		"json-pointer",
+		[]string{"application/json"},
+		"/name",
+		"title",
+	}
+	rules = append(rules, rule)
 
 	domainConfig := DomainConfig{
 		converters,
-        rules,
+		rules,
 	}
 	fmt.Printf("domainConfig: %v\n", domainConfig)
 
-    if converter := findConverter(url, domainConfig); converter != nil {
-        url = convertUrl(url, converter)
-        fmt.Println(url)
-        //@TODO: reload new DomainConfig for url
-    }
-
-    urlConfig := buildUrlConfig(url, &domainConfig)
-    fmt.Printf("%v\n", urlConfig.parsingRules)
+	return domainConfig
 }
-
-// @TODO: a method of the UrlConverter struct ?
-func findConverter(url string, config DomainConfig) *UrlConverter {
-   for _, converter := range config.converters {
-        matched, err := regexp.MatchString(converter.origin, url)
-        if matched {
-            return &converter
-        }
-        if err != nil {
-            //@TODO: do something with err (log.error)
-            fmt.Printf("err %v", err)
-            return nil
-        }
-   }
-
-   return nil
-}
-
-// @TODO: a method of the UrlConverter struct ?
-func convertUrl(url string, converter *UrlConverter) string {
-    re := regexp.MustCompile(converter.origin);
-    return re.ReplaceAllString(url, converter.destination)
-}
-
-func buildUrlConfig(url string, domainConfig *DomainConfig) *UrlConfig {
-    rules := make([]ParsingRule, 0)
-    for _, rule := range domainConfig.parsingRules {
-        fmt.Printf("%s\n", rule.urlPattern)
-        matched, err := regexp.MatchString(rule.urlPattern, url)
-        if matched {
-            rules = append(rules, rule)
-        }
-        if err != nil {
-            //@TODO: do something with err (log.error)
-            fmt.Printf("err %v", err)
-            return nil
-        }
-    }
-
-    urlConfig := &UrlConfig{rules}
-    return urlConfig
-}
-
